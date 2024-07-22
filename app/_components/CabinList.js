99bca060ae0305c7d00b8f4d7c0302e6d65fb0a5
Oceon101 <osaosula@yahoo.com>
2024-07-22 15:03:29 +0100

    First commit

---
+++ app/_components/CabinList.js
@@ -0,0 +1,26 @@
+import CabinCard from "./CabinCard";
+import { getCabins } from "../_lib/data-service";
+async function CabinList({ filter }) {
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  let displayCabins = cabins;
+  if (filter === "small")
+    displayCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter === "medium")
+    displayCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity < 8
+    );
+  if (filter === "large")
+    displayCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {displayCabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CabinList;
